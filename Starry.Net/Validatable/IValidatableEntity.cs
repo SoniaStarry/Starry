15feffca1cb3bfaf29ab6703401e1ca283149d70
Sun Bo <starry@vip.qq.com>
2015-05-05 18:03:23 +0800

    validatable request init

---
+++ Starry.Net/Validatable/IValidatableEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Starry.Net.Validatable
+{
+    public interface IValidatableEntity
+    {
+        string SecurityClass { set; get; }
+        int Version { set; get; }
+    }
+}
---
+++ Starry.Net/Validatable/ValidatableEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Starry.Net.Validatable
+{
+    [Serializable]
+    public class ValidatableEntity
+    {
+        public int Version { set; get; }
+    }
+}
